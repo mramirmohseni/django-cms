af2513b626ed06bc90ec3f5b339882ac9f36e1da
Amir Mohseni <mramirmohseni@gmail.com>
2025-11-05 13:22:45 +0330

    routes: update root URL configuration

--- cms/urls.py
+++ cms/urls.py
@@ -18,6 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog.views import list_view, detail_view
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", list_view),
+    path("<int:year>/<int:month>/<int:day>/<slug:slug>", detail_view),
 ]
